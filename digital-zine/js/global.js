ee2d60428a5bea849bbb03cd15a06ea62d6b6330
wangz630 <70614862+wangz630@users.noreply.github.com>
2021-03-05 08:36:43 -0500

    Update global.js

--- digital-zine/js/global.js
+++ digital-zine/js/global.js
@@ -1,24 +1,24 @@
 window.addEventListener("load", function() {
 
-    function openNav() {
-            document.getElementById("sidenav").style.width = "150px";
-            document.body.style.backgroundColor = "#979797";
-            document.getElementById("menu").style.backgroundColor = "#979797";
-      }
+  var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-      function openNav1() {
+  function openNav() {
+    if(width <=500){
+        document.getElementById("sidenav").style.width = "150px";
+        document.body.style.backgroundColor = "#979797";
+        document.getElementById("menu").style.backgroundColor = "#979797";
+    }
+    else if(width >500 && width <= 1024){
         document.getElementById("sidenav").style.width = "200px";
         document.body.style.backgroundColor = "#979797";
         document.getElementById("menu").style.backgroundColor = "#979797";
-
-      }
-
-      function openNav2() {
-        document.getElementById("sidenav").style.width = "250px";
-        document.body.style.backgroundColor = "#979797";
-        document.getElementById("menu").style.backgroundColor = "#979797";
-
-      }
+    }
+    else if(width >1024){
+    document.getElementById("sidenav").style.width = "250px";
+    document.body.style.backgroundColor = "#979797";
+    document.getElementById("menu").style.backgroundColor = "#979797";
+    }
+  }
       
       function closeNav() {
         document.getElementById("sidenav").style.width = "0";
@@ -29,22 +29,7 @@ window.addEventListener("load", function() {
 
       var open = document.getElementById('open');
       var close = document.getElementById('close');
-      var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-      
-        if(width <= 500){
-            open.addEventListener('click', openNav);
-            close.addEventListener('click', closeNav);
-        }
-        if(width > 500 && width <= 1024){
-            open.addEventListener('click', openNav1);
-            close.addEventListener('click', closeNav);
-        }
-        if(width > 1024){
-          open.addEventListener('click', openNav2);
-          close.addEventListener('click', closeNav);
-      }
-    
-      
 
-      
+      open.addEventListener('click', openNav)
+      close.addEventListener('click', closeNav);
 })
