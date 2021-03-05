3fd802e9d5820c13260a156ed749233da1d7f52e
wangz630 <70614862+wangz630@users.noreply.github.com>
2021-03-05 06:13:39 -0500

    digital zine

---
+++ digital-zine/js/global1.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", function() {
+
+    function openNav() {
+        if(screen.width <=500){
+            document.getElementById("sidenav").style.width = "150px";
+            document.body.style.backgroundColor = "#979797";
+            document.getElementById("menu").style.backgroundColor = "#979797";
+        }
+        if(screen.width >500 && screen.width <= 1024){
+            document.getElementById("sidenav").style.width = "200px";
+            document.body.style.backgroundColor = "#979797";
+            document.getElementById("menu").style.backgroundColor = "#979797";
+        }else{
+        document.getElementById("sidenav").style.width = "250px";
+        document.body.style.backgroundColor = "#979797";
+        document.getElementById("menu").style.backgroundColor = "#979797";
+        }
+      }
+      
+      function closeNav() {
+        document.getElementById("sidenav").style.width = "0";
+        document.getElementById("menu").style.marginLeft= "0";
+        document.body.style.backgroundColor = "black";
+        document.getElementById("menu").style.backgroundColor = "black";
+      }
+
+      var open = document.getElementById('open');
+      var close = document.getElementById('close');
+
+      open.addEventListener('click', openNav)
+      close.addEventListener('click', closeNav);
+})
