b62af6ca10ec7f1c0d21788293c05bd474cf3d97
wangz630 <70614862+wangz630@users.noreply.github.com>
2021-05-06 06:34:13 -0400

    project 4

--- project-4/js/global.js
+++ project-4/js/global.js
@@ -264,7 +264,6 @@ var keyMap = {
       keyMap[note].audio = undefined;
     }
     // Play the note
-    // var noteURL = 'https://itu.dk/people/tiwr/piano/' + note + '.mp3';
     var noteURL = './sound/' + note + '.mp3';
     var noteSound = new Audio(noteURL);
     keyMap[note].audio = noteSound;
@@ -297,7 +296,6 @@ var keyMap = {
 // Generate shapes for each note repeatedly
   var keys = document.querySelectorAll('.key');
   document.addEventListener("keypress", function(){
-
     keys.forEach((key) =>{
       if(key.classList.contains("C4") && key.classList.contains("pressed")){
         console.log("C4 press");
@@ -308,11 +306,13 @@ var keyMap = {
         shapeC4.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("Db4") && key.classList.contains("pressed")){
         console.log("Db4 press");
-        var shapeDb4 = document.createElement("div");
-        shapeDb4.classList.add("shapedb4");
-        document.querySelector(".shape-container").appendChild(shapeDb4);
-        shapeDb4.style.left = (95*Math.random())+'%';
-        shapeDb4.style.top=(95*Math.random()) +'%';
+        var shapeD4 = document.querySelectorAll(".shaped4");
+        shapeD4.forEach((shape)=>{
+          shape.style.width = "90px";
+          shape.style.height = "90px";
+          shape.style.left = (95*Math.random())+'%';
+          shape.style.top=(95*Math.random()) +'%';
+        })
       }else if(key.classList.contains("D4") && key.classList.contains("pressed")){
         console.log("D4 press");
         var shapeD4 = document.createElement("div");
@@ -343,25 +343,29 @@ var keyMap = {
         shapeF4.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("Gb4") && key.classList.contains("pressed")){
         console.log("Gb4 press");
-        var shapeGb4 = document.createElement("div");
-        shapeGb4.classList.add("shapegb4");
-        document.querySelector(".shape-container").appendChild(shapeGb4);
-        shapeGb4.style.left = (95*Math.random())+'%';
-        shapeGb4.style.top=(95*Math.random()) +'%';
+        var shapeD4 = document.querySelectorAll(".shaped4");
+        shapeD4.forEach((shape)=>{
+          shape.style.border ="2px solid #4B8585";
+          shape.style.left = (95*Math.random())+'%';
+          shape.style.top=(95*Math.random()) +'%';
+        })
       }else if(key.classList.contains("G4") && key.classList.contains("pressed")){
         console.log("G4 press");
-        var shapeG4 = document.createElement("div");
-        shapeG4.classList.add("shapeg4");
-        document.querySelector(".shape-container").appendChild(shapeG4);
-        shapeG4.style.left = (95*Math.random())+'%';
-        shapeG4.style.top=(95*Math.random()) +'%';
+        var shapeC4 = document.querySelectorAll(".shapec4");
+        shapeC4.forEach((shape)=>{
+          shape.style.backgroundColor = "#D45F82";
+          shape.style.left = (95*Math.random())+'%';
+          shape.style.top=(95*Math.random()) +'%';
+        })
       }else if(key.classList.contains("Ab4") && key.classList.contains("pressed")){
         console.log("Ab4 press");
-        var shapeAb4 = document.createElement("div");
-        shapeAb4.classList.add("shapeab4");
-        document.querySelector(".shape-container").appendChild(shapeAb4);
-        shapeAb4.style.left = (95*Math.random())+'%';
-        shapeAb4.style.top=(95*Math.random()) +'%';
+        var shapeE4 = document.querySelectorAll(".shapee4");
+        shapeE4.forEach((shape)=>{
+          shape.style.width = "120px";
+          shape.style.height = "120px";
+          shape.style.left = (95*Math.random())+'%';
+          shape.style.top=(95*Math.random()) +'%';
+        })
       }else if(key.classList.contains("A4") && key.classList.contains("pressed")){
         console.log("A4 press");
         var shapeA4 = document.createElement("div");
@@ -371,11 +375,13 @@ var keyMap = {
         shapeA4.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("Bb4") && key.classList.contains("pressed")){
         console.log("Bb4 press");
-        var shapeBb4 = document.createElement("div");
-        shapeBb4.classList.add("shapebb4");
-        document.querySelector(".shape-container").appendChild(shapeBb4);
-        shapeBb4.style.left = (95*Math.random())+'%';
-        shapeBb4.style.top=(95*Math.random()) +'%';
+        var shapeE5 = document.querySelectorAll(".shapee5");
+        shapeE5.forEach((shape)=>{
+          shape.style.width = "120px";
+          shape.style.height = "120px";
+          shape.style.left = (95*Math.random())+'%';
+          shape.style.top=(95*Math.random()) +'%';
+        })
       }else if(key.classList.contains("B4") && key.classList.contains("pressed")){
         console.log("B4 press");
         var shapeB4 = document.createElement("div");
@@ -399,11 +405,15 @@ var keyMap = {
         shapeDb5.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("D5") && key.classList.contains("pressed")){
         console.log("D5 press");
-        var shapeD5 = document.createElement("div");
-        shapeD5.classList.add("shaped5");
-        document.querySelector(".shape-container").appendChild(shapeD5);
-        shapeD5.style.left = (95*Math.random())+'%';
-        shapeD5.style.top=(95*Math.random()) +'%';
+        var shapeA4 = document.querySelectorAll(".shapea4");
+        shapeA4.forEach((shape)=>{
+          shape.style.width = "30px";
+          shape.style.height = "30px";
+          shape.style.border ="2px solid #a4c58f";
+          shape.style.backgroundColor ="transparent";
+          shape.style.left = (95*Math.random())+'%';
+          shape.style.top=(95*Math.random()) +'%';
+        })
       }else if(key.classList.contains("Eb5") && key.classList.contains("pressed")){
         console.log("Eb5 press");
         var shapeEb5 = document.createElement("div");
@@ -420,123 +430,121 @@ var keyMap = {
         shapeE5.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("Small-Rack-Tom") && key.classList.contains("pressed")){
         console.log("Small-Rack-Tom press");
-        var shapeC4 = document.querySelectorAll(".shapec4");
-        shapeC4.forEach((shape)=>{
-          shape.style.backgroundColor ="#BC7ED1";
-          shape.style.left = (95*Math.random())+'%';
-          shape.style.top=(95*Math.random()) +'%';
-          // shape.style.backgroundColor ="#" + ("00000" + ((Math.random() * 16777215 + 0.5) >> 0).toString(16)).slice(-6);
-        })
+        var shapeSrt = document.createElement("div");
+        shapeSrt.classList.add("shapesrt");
+        document.querySelector(".shape-container").appendChild(shapeSrt);
+        shapeSrt.style.left = (95*Math.random())+'%';
+        shapeSrt.style.top=(95*Math.random()) +'%';
+        
       }else if(key.classList.contains("Big-Rack-Tom") && key.classList.contains("pressed")){
         console.log("Big-Rack-Tom press");
-        var shapeDb4 = document.querySelectorAll(".shapedb4");
-        shapeDb4.forEach((shape)=>{
-          shape.style.backgroundColor ="#7E86D1";
-          shape.style.left = (95*Math.random())+'%';
-          shape.style.top=(95*Math.random()) +'%';
-        })
+        var shapeBrt = document.createElement("div");
+        shapeBrt.classList.add("shapebrt");
+        document.querySelector(".shape-container").appendChild(shapeBrt);
+        shapeBrt.style.left = (95*Math.random())+'%';
+        shapeBrt.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("Floor-Tom") && key.classList.contains("pressed")){
         console.log("Floor-Tom press");
-        var shapeD4 = document.querySelectorAll(".shaped4");
-        shapeD4.forEach((shape)=>{
-          shape.style.border ="2px solid #4B8585";
-          shape.style.left = (95*Math.random())+'%';
-          shape.style.top=(95*Math.random()) +'%';
-        })
+        var shapeFt = document.createElement("div");
+        shapeFt.classList.add("shapeft");
+        document.querySelector(".shape-container").appendChild(shapeFt);
+        shapeFt.style.left = (95*Math.random())+'%';
+        shapeFt.style.top=(95*Math.random()) +'%';
+
+
       }else if(key.classList.contains("High-Hat-Open") && key.classList.contains("pressed")){
         console.log("High-Hat-Open press");
-        var shapeEb4 = document.querySelectorAll(".shapeeb4");
-        shapeEb4.forEach((shape)=>{
-          shape.style.border ="none";
-          shape.style.backgroundColor ="#e76161";
-          shape.style.left = (95*Math.random())+'%';
-          shape.style.top=(95*Math.random()) +'%';
-        })
+        var shapeHho = document.createElement("div");
+        shapeHho.classList.add("shapehho");
+        document.querySelector(".shape-container").appendChild(shapeHho);
+        shapeHho.style.left = (95*Math.random())+'%';
+        shapeHho.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("Hi-Hat-Closed") && key.classList.contains("pressed")){
         console.log("Hi-Hat-Closed press");
-        var shapeE4 = document.querySelectorAll(".shapee4");
-        shapeE4.forEach((shape)=>{
-          shape.style.width = "150px";
-          shape.style.height = "150px";
-          shape.style.left = (95*Math.random())+'%';
-          shape.style.top=(95*Math.random()) +'%';
-        })
+        var shapeHhc = document.createElement("div");
+        shapeHhc.classList.add("shapehhc");
+        document.querySelector(".shape-container").appendChild(shapeHhc);
+        shapeHhc.style.left = (95*Math.random())+'%';
+        shapeHhc.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("Kick") && key.classList.contains("pressed")){
         console.log("Kick press");
         var shapeF4 = document.querySelectorAll(".shapef4");
         shapeF4.forEach((shape)=>{
           shape.style.width = "100px";
           shape.style.height = "100px";
+          shape.style.borderRadius ="50% 50%";
           shape.style.backgroundColor ="#B99DF4";
           shape.style.left = (95*Math.random())+'%';
           shape.style.top=(95*Math.random()) +'%';
         })
       }else if(key.classList.contains("Snare") && key.classList.contains("pressed")){
         console.log("Snare press");
-        var shapeGb4 = document.querySelectorAll(".shapegb4");
-        shapeGb4.forEach((shape)=>{
-          shape.style.backgroundColor ="#EB7D7D";
-          shape.style.left = (95*Math.random())+'%';
-          shape.style.top=(95*Math.random()) +'%';
-        })
+        var shapeSn = document.createElement("div");
+        shapeSn.classList.add("shapesn");
+        document.querySelector(".shape-container").appendChild(shapeSn);
+        shapeSn.style.left = (95*Math.random())+'%';
+        shapeSn.style.top=(95*Math.random()) +'%';
+        
       }else if(key.classList.contains("Shaker") && key.classList.contains("pressed")){
         console.log("Shaker press");
-        var shapeG4 = document.querySelectorAll(".shapeg4");
-        shapeG4.forEach((shape)=>{
-          shape.style.border ="2px solid #f3df9e";
-          shape.style.backgroundColor ="transparent";
+       var shapeEb5 = document.querySelectorAll(".shapeeb5");
+        shapeEb5.forEach((shape)=>{
+          shape.style.width = "20px";
+          shape.style.height = "20px";
+          shape.style.borderRadius ="50% 50%";
           shape.style.left = (95*Math.random())+'%';
           shape.style.top=(95*Math.random()) +'%';
         })
       }else if(key.classList.contains("Clap") && key.classList.contains("pressed")){
         console.log("Clap press");
-        var shapeAb4 = document.querySelectorAll(".shapeab4");
-        shapeAb4.forEach((shape)=>{
-          shape.style.width = "200px";
-          shape.style.height = "100px";
+         var shapeE4 = document.querySelectorAll(".shapee4");
+        shapeE4.forEach((shape)=>{
+          shape.style.width = "150px";
+          shape.style.height = "150px";
+          shape.style.borderRadius ="50% 50%";
           shape.style.left = (95*Math.random())+'%';
           shape.style.top=(95*Math.random()) +'%';
         })
       }else if(key.classList.contains("Drum-Fill") && key.classList.contains("pressed")){
         console.log("Drum-Fill press");
-        var shapeA4 = document.querySelectorAll(".shapea4");
-        shapeA4.forEach((shape)=>{
-          shape.style.border ="2px solid #a4c58f";
-          shape.style.backgroundColor ="transparent";
-          // shape.style.left = (95*Math.random())+'%';
-          // shape.style.top=(95*Math.random()) +'%';
+         var shapeEb4 = document.querySelectorAll(".shapeeb4");
+        shapeEb4.forEach((shape)=>{
+          shape.style.border ="none";
+          shape.style.backgroundColor ="#e76161";
+          shape.style.width = "40px";
+          shape.style.height = "40px";
+          shape.style.borderRadius ="50% 50%";
+          shape.style.left = (95*Math.random())+'%';
+          shape.style.top=(95*Math.random()) +'%';
         })
       }else if(key.classList.contains("Piano-Chord") && key.classList.contains("pressed")){
         console.log("Piano-Chord press");
-        var shapeBb4 = document.querySelectorAll(".shapebb4");
-        shapeBb4.forEach((shape)=>{
-          shape.style.width = "200px";
-          shape.style.height = "200px";
-          shape.style.backgroundColor ="transparent";
+        var shapeF4 = document.querySelectorAll(".shapef4");
+        shapeF4.forEach((shape)=>{
+          shape.style.backgroundColor ="#FAEE81";
           shape.style.left = (95*Math.random())+'%';
           shape.style.top=(95*Math.random()) +'%';
         })
       }else if(key.classList.contains("Guitar-Low") && key.classList.contains("pressed")){
         console.log("Guitar-Low press");
-        var shapeB4 = document.querySelectorAll(".shapeb4");
-        shapeB4.forEach((shape)=>{
-          shape.style.width = "20px";
-          shape.style.height = "20px";
-          shape.style.left = (95*Math.random())+'%';
-          shape.style.top=(95*Math.random()) +'%';
-        })
+        var shapeGl = document.createElement("div");
+        shapeGl.classList.add("shapegl");
+        document.querySelector(".shape-container").appendChild(shapeGl);
+        shapeGl.style.left = (95*Math.random())+'%';
+        shapeGl.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("Guitar-High") && key.classList.contains("pressed")){
         console.log("Guitar-High press");
-        var shapeC5 = document.querySelectorAll(".shapec5");
-        shapeC5.forEach((shape)=>{
-          shape.style.backgroundColor ="#FBF683";
-          shape.style.left = (95*Math.random())+'%';
-          shape.style.top=(95*Math.random()) +'%';
-        })
+        var shapeGh = document.createElement("div");
+        shapeGh.classList.add("shapegh");
+        document.querySelector(".shape-container").appendChild(shapeGh);
+        shapeGh.style.left = (95*Math.random())+'%';
+        shapeGh.style.top=(95*Math.random()) +'%';
+      
       }else if(key.classList.contains("Guitar-Chord1") && key.classList.contains("pressed")){
         console.log("Guitar-Chord1 press");
         var shapeDb5 = document.querySelectorAll(".shapedb5");
         shapeDb5.forEach((shape)=>{
+          shape.style.width = '1px';
           shape.style.border ="2px solid #6E7CFF";
           shape.style.backgroundColor ="transparent";
           shape.style.left = (95*Math.random())+'%';
@@ -544,20 +552,19 @@ var keyMap = {
         })
       }else if(key.classList.contains("Guitar-Chord2") && key.classList.contains("pressed")){
         console.log("Guitar-Chord2 press");
-        var shapeD5 = document.querySelectorAll(".shaped5");
-        shapeD5.forEach((shape)=>{
-          shape.style.width = "30px";
-          shape.style.height = "30px";
-          shape.style.borderRadius = "50% 50%";
-          shape.style.left = (95*Math.random())+'%';
-          shape.style.top=(95*Math.random()) +'%';
-        })
+        var shapePc2 = document.createElement("div");
+        shapePc2.classList.add("shapepc2");
+        document.querySelector(".shape-container").appendChild(shapePc2);
+        shapePc2.style.left = (95*Math.random())+'%';
+        shapePc2.style.top=(95*Math.random()) +'%';
       }else if(key.classList.contains("Guitar-Chord3") && key.classList.contains("pressed")){
         console.log("Guitar-Chord3 press");
         var shapeEb5 = document.querySelectorAll(".shapeeb5");
         shapeEb5.forEach((shape)=>{
           shape.style.border ="none";
           shape.style.backgroundColor ="#95e2be";
+          shape.style.width ='3px';
+          shape.style.height = '50px';
           shape.style.left = (95*Math.random())+'%';
           shape.style.top=(95*Math.random()) +'%';
         })
@@ -566,7 +573,7 @@ var keyMap = {
         var shapeE5 = document.querySelectorAll(".shapee5");
         shapeE5.forEach((shape)=>{
           shape.style.width = "10px";
-          shape.style.height = "10px";
+          shape.style.height = "100px";
           shape.style.left = (95*Math.random())+'%';
           shape.style.top=(95*Math.random()) +'%';
         })
@@ -591,6 +598,7 @@ var keyMap = {
         document.querySelector("#note-indicator").classList.toggle("hide");
       }else if (event.keyCode == 8){
         console.log("backspace");
+        document.querySelector("#note-indicator").innerText = "Press any keys";
         var randomColor = [];
         index = 0;
         randomColor[0] = '#63617B';
@@ -672,6 +680,42 @@ var keyMap = {
     })
     var removeE5 = document.querySelectorAll('.shapee5');
     removeE5.forEach((e)=>{
+      e.remove();
+    })
+    var removeBrt = document.querySelectorAll('.shapebrt');
+    removeBrt.forEach((e)=>{
+      e.remove();
+    })
+    var removeGl = document.querySelectorAll('.shapegl');
+    removeGl.forEach((e)=>{
+      e.remove();
+    })
+    var removeSrt = document.querySelectorAll('.shapesrt');
+    removeSrt.forEach((e)=>{
+      e.remove();
+    })
+    var removeGh = document.querySelectorAll('.shapegh');
+    removeGh.forEach((e)=>{
+      e.remove();
+    })
+    var removePc2 = document.querySelectorAll('.shapepc2');
+    removePc2.forEach((e)=>{
+      e.remove();
+    })
+    var removeSn = document.querySelectorAll('.shapesn');
+    removeSn.forEach((e)=>{
+      e.remove();
+    })
+    var removeFt = document.querySelectorAll('.shapeft');
+    removeFt.forEach((e)=>{
+      e.remove();
+    })
+    var removeHho = document.querySelectorAll('.shapehho');
+    removeHho.forEach((e)=>{
+      e.remove();
+    })
+    var removeHhc = document.querySelectorAll('.shapehhc');
+    removeHhc.forEach((e)=>{
       e.remove();
     })
     // document.body.style.backgroundColor ="#6A4D59";
